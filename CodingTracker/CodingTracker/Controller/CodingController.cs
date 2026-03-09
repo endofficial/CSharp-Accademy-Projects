977f56357059c1de2e75f9e8408918ec6fc4079b
Andrea Verdirrame <andreaverdirrame11@gmail.com>
2026-03-09 23:25:42 +0100

    #I added ViewSessions methotd to get all sessions

--- CodingTracker/CodingTracker/Controller/CodingController.cs
+++ CodingTracker/CodingTracker/Controller/CodingController.cs
@@ -3,6 +3,8 @@ using CodingTracker;
 using Dapper;
 using Microsoft.Data.Sqlite;
 using CodingTracker.Data;
+using System.Data.SQLite;
+using CodingTracker.Model;
 
 namespace CodingTracker.Controller;
 
@@ -22,13 +24,54 @@ internal class CodingController : Database
         {
             using var connection = GetConnection();
 
-            // FERMO QUI: PROBLEMA CON LA REGISTRAZIONE DELLA TABELLA. NON TROVA I VALORI.
             string sql = @"
                 INSERT INTO CodingSessions (StartTime, EndTime, Date, Duration, Description)
                 VALUES (@StartTime, @EndTime, @Date, @Duration, @Description);";
 
             connection.Execute(sql, durationSession);
         }
+
+        return true;
+    }
+
+    public static bool ViewSessions()
+    {
+        Clear();
+
+        using var connection = GetConnection();
+
+        string sql = @"
+            SELECT * FROM CodingSessions";
+
+        List<CodingSessions> tableData = new List<CodingSessions>();
+
+        var sessions = connection.Query<CodingSessions>(sql).ToList(); // Execute the query and map results to CodingSessions objects
+
+        var table = new Table();
+        table.Border(TableBorder.Rounded);
+
+        table.AddColumn("[yellow]ID[/]");
+        table.AddColumn("[yellow]Start Time[/]");
+        table.AddColumn("[yellow]End Time[/]");
+        table.AddColumn("[yellow]Date[/]");
+        table.AddColumn("[yellow]Duration[/]");
+        table.AddColumn("[yellow]Description[/]");
+
+        foreach (var session in sessions)
+        {
+            table.AddRow(
+                session.Id.ToString(),
+                session.StartTime.ToString("HH:mm"),
+                session.EndTime.ToString("HH:mm"),
+                session.Date,
+                session.Duration.ToString(@"hh\:mm"),
+                (session.Description ?? "Empty").ToString()
+                );
+        }
+
+        AnsiConsole.Write(table);
+        AnsiConsole.MarkupLine("\n[yellow]Press any key to continue...[/]");
+        Console.ReadKey();
 
         return true;
     }
